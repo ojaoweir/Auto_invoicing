cfaaf8d7783114e312e6d852c6d62f7495ded21f
Isak Jansson <isaja339@student.liu.se>
2018-05-17 23:16:48 +0200

    Totalprice filed working correctly, i think

--- program/rendering.py
+++ program/rendering.py
@@ -8,10 +8,12 @@ def generateInvoiceTemplate(invoice):
     sender = dbGetSender(invoice.sender_id)
     customer = dbGetCustomer(invoice.customer_id)
     services = invoice.payed_service
+    total_price_for_all = invoice.price
     env = Environment(loader=FileSystemLoader('.'))
     template = env.get_template("program/templates/invoice.html")
     template_variables = {"sender": sender,
                         "customer": customer,
                         "invoice": invoice,
-                        "services":services}
+                        "services":services,
+                        "total_price_for_all": total_price_for_all}
     return template.render(template_variables)
